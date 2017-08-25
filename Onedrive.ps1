665ec9f5cf1b99e5f28d435de7b36511e00b9284
bantoilatoi <bantoilatoi@gmail.com>
2017-08-25 23:46:23 +0700

    master

---
+++ Onedrive.ps1
@@ -0,0 +1,6 @@
+﻿Clear-Host
+$x86 = "%SYSTEMROOT%\System32\OneDriveSetup.exe"
+$x64 = "%SYSTEMROOT%\SysWOW64\OneDriveSetup.exe"
+Write-Host 'Close Onedrive process'
+Get-Process
+Ping 127.0.0.1  > NULL 2>&1
--- remove_all_app_win_10.ps1
+++ remove_all_app_win_10.ps1
@@ -4,18 +4,18 @@ if((Test-Path -Path $pathfileMyApps) -eq $False ){
     write-host 'Create file myapps.txt'
     $Location = "$env:userprofile\Desktop\"
     New-Item -Path $Location -Name "myapps.txt" -ItemType File
-    Remove-Item $pathfileMyApps -Recurse -Force -ErrorAction
+   #Remove-Item $pathfileMyApps -Recurse -Force -ErrorAction
 }
 
 Get-AppxPackage | Select PackageFullName > $pathfileMyApps
 $reader = [System.IO.File]::OpenText($pathfileMyApps)
 while($null -ne ($line = $reader.ReadLine())) {
 	remove-AppxProvisionedPackage -online -packagename $line
-	remove-AppxPackage -package $line
+	remove-AppxPackage -package $line Continue
 }
 try
 {
-	Remove-Item $pathfileMyApps -Recurse -Force -ErrorAction
+	Remove-Item $pathfileMyApps -Recurse -Force -ErrorAction Continue
 }
 catch
 {
